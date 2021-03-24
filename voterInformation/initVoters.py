66da775d172a3a17513c6c0431bf43815cbc0178
Vikram Krishna <vikramkrishna4444@gmail.com>
2021-03-25 02:05:01 +0800

    Completed Setting Up Voter Checking Framework

--- voterInformation/initVoters.py
+++ voterInformation/initVoters.py
@@ -1,33 +1,36 @@
 import pandas as pd
-import random
+from voterVerifier import voterVerifierClass
+from makeshiftHashNew import hasher
 
-from ..globalVals import globalValsClass
 class initVoters:
+    def __init__ (self):
+        self.initHasher = hasher()
 
-    def getDobNum(self, ogValue, index):
-        try:
-            return int(ogValue[index])
-        except:
-            return 0
-    
-    def __init__(self):
-        votersDf = pd.read_csv("/Volumes/Vikram's Hard Drive/Programming/chainVote/voterInformation/registeredVoters.csv")
-        votersDf.insert(loc=len(votersDf.columns), column="Hash", value = "hash")
-        votersDf = votersDf.astype({'Date of Birth': 'datetime64[ns, US/Eastern]'})
-        votersDf['Date of Birth'] = votersDf['Date of Birth'].dt.date
+        # windows:
+        self.verifVals = pd.read_csv(r"C:/Users/anonymousvikram/Documents/GitHub/chainVote/globalVals.csv")
+        self.votersDf = pd.read_csv(r"C:/Users/anonymousvikram/Documents/GitHub/chainVote/voterInformation/registeredVoters.csv")
+        
+        # macOS:
+        # self.verifVals = pd.read_csv("/Volumes/Vikram's Hard Drive/Programming/chainVote/globalVals.csv")
+        # self.votersDf = pd.read_csv("/Volumes/Vikram's Hard Drive/Programming/chainVote/voterInformation/registeredVoters.csv")
+        
+        # Setting up date for dob
+        self.votersDf.insert(loc=len(self.votersDf.columns), column="Hash", value = "hash")
+        self.votersDf = self.votersDf.astype({'Date of Birth': 'datetime64[ns, US/Eastern]'})
+        self.votersDf['Date of Birth'] = self.votersDf['Date of Birth'].dt.date
 
-        # Setting Random Vars
-        dobAdder = votersDf['Date of Birth'][0].year
-        dobAdder += self.getDobNum(str(votersDf['Date of Birth'][0].month), dobM) * dobMModifier
-        dobAdder += self.getDobNum(str(votersDf['Date of Birth'][0].day), dobD) * dobDModifier
-        dobAdder += dobOverallModifier
+        self.voterVerificationSystem = voterVerifierClass(self.verifVals)
 
-        tempId = ""
-        tempId = tempId + votersDf['First Name'][0][fNameStart : fNameStart + fNameLen]
-        tempId = tempId + votersDf['Last Name'][0][lNameStart : lNameStart + lNameLen]
-        tempId = tempId + str(dobAdder)
-        tempId = tempId + votersDf['Identification'][0][idStart : idStart + idLen]
-        tempId = tempId + str(votersDf['Phone Number'][0])[pnStart : pnStart + pnLen]
+        self.votersDf['Hash'] = 'tempHash'
+        
+        for index, voter in self.votersDf.iterrows():
+            preHash = self.voterVerificationSystem.processVoter(voter)
+            postHash = self.initHasher.allInOneHash(preHash)
+            self.votersDf.loc[self.votersDf['First Name'] == voter.loc['First Name'], 'Hash'] = postHash
+        
+        print(self.votersDf['Hash'])
 
-        approvedVoters[0] = tempId
-        print(approvedVoters)
+        approvedHashes = self.votersDf['Hash']
+        approvedHashes.to_csv(r"C:/Users/anonymousvikram/Documents/GitHub/chainVote/voterInformation/approvedHash.csv", index = 0)
+
+votersTest = initVoters()
